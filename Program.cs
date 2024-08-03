659d955939685baa06d69d216489ee6390699556
KettLovahr <catusfelony@gmail.com>
2024-08-03 01:46:25 -0300

    Windows-specific improvements

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ namespace temps
         [STAThread]
         public static void Main(string[] args)
         {
+            if (OperatingSystem.IsWindows()) {
+                // Adds the bin directory to the Path env variable so that it can find the GTK libraries
+                // This solves the problem with deploying GTK# applications to Windows
+                string systemPath = Environment.GetEnvironmentVariable("Path");
+                Environment.SetEnvironmentVariable("Path", $"{(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin"))};{systemPath}");
+                // This makes GTK# applications use the native Windows theme
+                Environment.SetEnvironmentVariable("GTK_THEME", "win32");
+            }
+
             Application.Init();
 
             var app = new Application("org.temps.temps", GLib.ApplicationFlags.None);
